d6cc5c4ed9de955b1d70a1bf65e91a1363828af6
woohyeon5 <148295632+woohyeon5@users.noreply.github.com>
2024-03-09 18:34:54 +0900

    [level 0] Title: 가까운 수, Time: 0.32 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -1,15 +1,14 @@
-import java.util.Arrays;
+import java.util.*;
 class Solution {
     public int solution(int[] array, int n) {
         Arrays.sort(array);
-        int answer = 0;
-        int num = n+100;
-        for (int i = 0; i < array.length; i++) {
-          if (Math.abs(array[i] - n) < num) {
-            num = Math.abs(array[i] - n);
-            answer = i;
-          }
+        
+        for(int i = 1 ; i < array.length ; i++){
+            if(Math.abs(n-array[0]) > Math.abs(n-array[i])){
+                array[0] = array[i];
+            }
         }
-        return array[answer];
+        
+        return array[0];
     }
 }
